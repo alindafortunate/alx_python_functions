f0052ab043388922ca48bbc1f0824f2b961d5cd4
alindafortunate <alindafortunate5@gmail.com>
2025-01-06 11:52:03 +0300

    Initial commit

---
+++ area.py
@@ -0,0 +1,9 @@
+def calculate_area(length, width):
+    """calculates the area of the rectangle."""
+    area = length * width
+    return area
+
+
+length = 5.6
+width = 4.2
+print(f"The area is {calculate_area(length, width)} cm.")
---
+++ greetings.py
@@ -0,0 +1,15 @@
+def greet(name):
+    """prints a greeting message"""
+    print(f"Hello, {name}!")
+
+
+greet("Alinda")
+
+
+add = lambda x, y: x + y
+result = add(3, 5)
+print(result)
+
+multiply = lambda y, z: y * z
+result = multiply(5, 3)
+print(result)
